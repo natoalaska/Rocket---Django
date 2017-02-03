efa2585ef3ae442810df08f65df381c48c706d0e
Nathan Vander Martin <natoalaska@gmail.com>
2017-02-03 10:55:09 -0900

    Added Polls

--- manage.py
+++ manage.py
@@ -5,6 +5,9 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rocket.settings")
     try:
+        from django.core.management.commands.runserver import Command as runserver
+        runserver.default_port = "80"
+        
         from django.core.management import execute_from_command_line
     except ImportError:
         # The above import may fail for some other reason. Ensure that the
