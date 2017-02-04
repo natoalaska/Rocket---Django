9a4f7d6f943bd12c8659a36afbd003ed722ecb03
Nathan <natoalaska@NVMM.local>
2017-02-04 08:31:05 -0900

    Start work on adding bootstrap to admin

--- rocket/settings.py
+++ rocket/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
